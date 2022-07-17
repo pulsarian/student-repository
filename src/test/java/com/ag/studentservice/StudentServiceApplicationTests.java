08d90ea8d231219951ccd52edec7c2f4d0c625cc
Abhishek Gorai <gorai.abhi@gmail.com>
2022-07-18 01:26:36 +0530

    Initial commit for student-service

---
+++ src/test/java/com/ag/studentservice/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
